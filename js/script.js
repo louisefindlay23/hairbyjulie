6c93a07c5dfbf7a6841d2c46a317eadce22aa343
louisefindlay23 <louisefindlay23@outlook.com>
2019-02-20 19:46:14 +0000

    Contact form (minus submit) complete

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// JavaScript Document
+
+$( document ).ready(function() {
+    
+    $('.main-carousel').flickity({
+    
+      cellAlign: 'left',
+      wrapAround: true,
+      autoPlay: 1500
+});
+    
+});
