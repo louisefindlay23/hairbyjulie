e0317ea40e151d672b3baa9b1fd75136ee8870ed
louisefindlay23 <louisefindlay23@outlook.com>
2019-02-21 10:00:20 +0000

    Got the show/hide working

--- js/script.js
+++ js/script.js
@@ -1,6 +1,25 @@
 // JavaScript Document
 
 $( document ).ready(function() {
+
+    $("#team2").hide();
     
+    $("#dot1").click(function(){
+        
+        $(".team").hide();
+        
+        $("#team1").show();
+        
+        $(this).addClass("pink");
+        
+    });
+    
+    $("#dot2").click(function(){
+        
+        $(".team").hide();
+        
+        $("#team2").show();
+        
+    });
  
 });
